605cdc014abd43cda035c514ce5dcbc9dc18bfba
AutomatedTester <dburns@mozilla.com>
2014-07-19 21:44:55 +0100

    correct cmd signature

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
+from mercurial import commands
+
+def malone():
+    pass
+
 cmdtable = {
-    "malone": (
-            [("g", "get", "", "Get all bugs assigned to you")]
+    "malone": ( malone,
+            [("g", "get", "", "Get all bugs assigned to you")],
+            "Keep all your bugs with you so you can keep on working without internet"
         )
 }
